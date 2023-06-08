7caeadb4301e5dc4845310dc3c917bcc08544bc9
Hamza Azman <hamzaazman@outlook.com>
2023-06-08 18:09:48 +0300

    refactor the handle data ways in detail

--- app/src/main/java/com/hamzaazman/kotlinfreetoplay/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/hamzaazman/kotlinfreetoplay/ui/detail/DetailFragment.kt
@@ -2,7 +2,6 @@ package com.hamzaazman.kotlinfreetoplay.ui.detail
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -15,6 +14,7 @@ import coil.load
 import com.hamzaazman.kotlinfreetoplay.R
 import com.hamzaazman.kotlinfreetoplay.common.viewBinding
 import com.hamzaazman.kotlinfreetoplay.databinding.FragmentDetailBinding
+import com.hamzaazman.kotlinfreetoplay.domain.model.GameDetailUi
 import com.hamzaazman.kotlinfreetoplay.extractYearFromDateString
 import com.hamzaazman.kotlinfreetoplay.makeCollapsible
 import dagger.hilt.android.AndroidEntryPoint
@@ -39,84 +39,92 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
         }
 
         detailUiState()
-
-        binding.detailToolbar.setNavigationOnClickListener {
-            findNavController().popBackStack()
-        }
+        goBackStack()
 
     }
 
-    private fun detailUiState() = with(binding) {
+    private fun goBackStack() = with(binding) {
+        detailToolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun handleDetailUiState(response: DetailUiState) = with(binding) {
+        when (response) {
+            is DetailUiState.Loading -> {
+                shimmerDetailContainer.startShimmer()
+                shimmerDetailContainer.visibility = View.VISIBLE
+                nestedScrollView.isNestedScrollingEnabled = false
+            }
+
+            is DetailUiState.Error -> {
+                shimmerDetailContainer.stopShimmer()
+                shimmerDetailContainer.visibility = View.GONE
+                nestedScrollView.isNestedScrollingEnabled = false
+            }
+
+            is DetailUiState.Success -> {
+                nestedScrollView.isNestedScrollingEnabled = true
+                shimmerDetailContainer.stopShimmer()
+                shimmerDetailContainer.visibility = View.GONE
+                setDetailData(response.data)
+            }
+        }
+    }
+
+    private fun setDetailData(detailResult: GameDetailUi) = with(binding) {
+        shimmerDetailContainer.stopShimmer()
+        shimmerDetailContainer.visibility = View.GONE
+        detailImageView.load(detailResult.thumbnail) {
+            crossfade(true)
+            placeholder(R.drawable.game_placeholder)
+        }
+
+        detailDesc.makeCollapsible(3, Int.MAX_VALUE, expandMoreDrawable)
+
+        detailTitle.text = detailResult.title
+        detailGenre.text = detailResult.genre
+        detailPlatform.text = detailResult.platform
+        detailReleaseDate.text =
+            detailResult.releaseDate?.extractYearFromDateString()
+        detailDesc.text = detailResult.description
+
+        if (detailResult.minimumSystemRequirements == null) {
+            systemReqConstraintLayout.visibility = View.GONE
+            viewLineAbout.visibility = View.GONE
+            viewLineSystemReq.visibility = View.GONE
+        }
+        systemReqOS.text = detailResult.minimumSystemRequirements?.os
+        systemReqCPU.text =
+            detailResult.minimumSystemRequirements?.processor ?: ""
+        systemReqRAM.text = detailResult.minimumSystemRequirements?.memory ?: ""
+        systemReqStorage.text =
+            detailResult.minimumSystemRequirements?.storage ?: ""
+        systemReqGraphics.text =
+            detailResult.minimumSystemRequirements?.graphics ?: ""
+
+        screenshotRecyclerView.adapter = reviewAdapter
+        reviewAdapter.submitList(detailResult.screenshots ?: emptyList())
+
+        if (detailResult.screenshots.isNullOrEmpty()) {
+            screenshotTitle.visibility = View.GONE
+        }
+
+        nestedScrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
+
+            if (scrollY >= detailTitle.top + detailTitle.height) {
+                detailToolbar.title = detailTitle.text
+            } else {
+                detailToolbar.title = ""
+            }
+        })
+    }
+
+    private fun detailUiState() {
         viewLifecycleOwner.lifecycleScope.launch {
             vm.detailData.collect { response ->
-                when (response) {
-                    is DetailUiState.Loading -> {
-                        shimmerDetailContainer.startShimmer()
-                        shimmerDetailContainer.visibility = View.VISIBLE
-                    }
-
-                    is DetailUiState.Error -> {
-                        shimmerDetailContainer.stopShimmer()
-                        shimmerDetailContainer.visibility = View.GONE
-                        Toast.makeText(requireContext(), response.message, Toast.LENGTH_SHORT)
-                            .show()
-                    }
-
-                    is DetailUiState.Success -> {
-                        shimmerDetailContainer.stopShimmer()
-                        shimmerDetailContainer.visibility = View.GONE
-                        response.data.let { detailResult ->
-                            detailImageView.load(detailResult.thumbnail) {
-                                crossfade(true)
-                                placeholder(R.drawable.game_placeholder)
-                            }
-
-                            detailDesc.makeCollapsible(3, Int.MAX_VALUE, expandMoreDrawable)
-
-                            detailTitle.text = detailResult.title
-                            detailGenre.text = detailResult.genre
-                            detailPlatform.text = detailResult.platform
-                            detailReleaseDate.text =
-                                detailResult.releaseDate?.extractYearFromDateString()
-                            detailDesc.text = detailResult.description
-
-                            if (detailResult.minimumSystemRequirements == null) {
-                                systemReqConstraintLayout.visibility = View.GONE
-                                viewLineAbout.visibility = View.GONE
-                                viewLineSystemReq.visibility = View.GONE
-                            }
-                            systemReqOS.text = detailResult.minimumSystemRequirements?.os
-                            systemReqCPU.text =
-                                detailResult.minimumSystemRequirements?.processor ?: ""
-                            systemReqRAM.text = detailResult.minimumSystemRequirements?.memory ?: ""
-                            systemReqStorage.text =
-                                detailResult.minimumSystemRequirements?.storage ?: ""
-                            systemReqGraphics.text =
-                                detailResult.minimumSystemRequirements?.graphics ?: ""
-
-                            screenshotRecyclerView.adapter = reviewAdapter
-                            reviewAdapter.submitList(detailResult.screenshots ?: emptyList())
-
-                            if (detailResult.screenshots.isNullOrEmpty()) {
-                                screenshotTitle.visibility = View.GONE
-                            }
-
-                            nestedScrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
-
-                                if (scrollY >= detailTitle.top + detailTitle.height) {
-                                    detailToolbar.title = detailTitle.text
-                                } else {
-                                    detailToolbar.title = ""
-                                }
-                            })
-
-
-                        }
-                    }
-
-                }
+                handleDetailUiState(response)
             }
         }
     }
 }
-
